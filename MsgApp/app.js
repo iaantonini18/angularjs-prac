26312b53c3602b2d726ba509adb9a9cb6e76d218
Isabella Antonini <isabellaantonini@Isas-MacBook.hsd1.il.comcast.net>
2020-05-27 16:30:03 -0500

    MsgApp

--- MsgApp/app.js
+++ MsgApp/app.js
@@ -1,11 +1,15 @@
-(funtion () {
-'use strict';
+(function() {
+    "use strict";
 
-angular.module('MsgApp', [])
-    .controller('MsgController', MsgController);
-
-    MsgController.$inject = ['$scope'];
-    function MsgController($scope) {
-        $scope.name = "Isa"
-    }
-}   
+    angular.module('MsgApp', [])
+    .controller('MsgController', function ($scope) {
+          $scope.name = "Isa";
+          $scope.stateOfBeing = "hangry";
+          $scope.sayMessage = function() {
+              return $scope.name + " likes snaaaacks"
+          };
+          $scope.feed = function() {
+            $scope.stateOfBeing = "happy";
+          };
+      })
+})();
